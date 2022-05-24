8a370d53340e1738f495e7341821272977824d47
blublin <lublin.ben@gmail.com>
2022-05-24 09:43:20 -0700

    initial commit

---
+++ canBecomePalindrome/index.js
@@ -0,0 +1,84 @@
+/* 
+  Given to me (Neil) in an interview
+  Given a string
+  return whether or not it's possible to make a palindrome out of the string's
+  characters.
+  What is it about a string that makes it possible for it to become a
+  palindrome?
+*/
+
+const str1 = "";
+const expected1 = false;
+
+const str2 = "a";
+const expected2 = true;
+
+const str3 = "ddaa";
+const expected3 = true;
+// Explanation: "daad" or "adda"
+
+const str4 = "dda";
+const expected4 = true;
+// Explanation: "dad"
+
+const str5 = "aaadd";
+const expected5 = true;
+// Explanation: "daaad"
+
+const str6 = "abc";
+const expected6 = false;
+
+/**
+ * Determines whether or not it is possible for the string's characters to be
+ * rearranged into a palindrome.
+ * - Space: O(?).
+ * - Time: O(?).
+ * @param {string} str
+ * @returns {boolean} Whether the given str can be rearranged into a palindrome.
+ */
+function canStringBecomePalindrome(str) {
+    if (str.length === 0) return false;
+    let oddCount = 0
+
+    const freqTbl = {}
+    for (let char of str)  {
+        if (freqTbl[char]) {
+            freqTbl[char]++;
+        }
+        else {
+            freqTbl[char] = 1;
+        }
+
+        if (freqTbl[char]%2) {
+            oddCount++;
+        }
+        else {
+            oddCount--;
+        }
+    }
+    // console.log(Object.values(freqTbl))
+    // for (const v of Object.values(freqTbl)) {
+    //     console.log(`V now is: ${v}`)
+    //     if (v%2) {
+    //         console.log("Triggered odd number")
+    //         oddCount++
+    //         console.log(`Odd Count: ${oddCount}`)
+    //         if (oddCount > 1) {
+    //             return false
+    //         }
+    //     }
+    // }
+    return oddCount>1 ? false : true
+}
+
+console.log(canStringBecomePalindrome(str1))
+console.log("\n\n")
+console.log(canStringBecomePalindrome(str2))
+console.log("\n\n")
+console.log(canStringBecomePalindrome(str3))
+console.log("\n\n")
+console.log(canStringBecomePalindrome(str4))
+console.log("\n\n")
+console.log(canStringBecomePalindrome(str5))
+console.log("\n\n")
+console.log(canStringBecomePalindrome(str6))
